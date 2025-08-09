b731c6809ef67716e73bdc24ac4409f238b25080
Sharjil Siddiqui <sharjilsidd187@gmail.com>
2025-08-10 01:22:36 +0530

    Vercel Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
-// export default defineConfig({
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+  base: './', // or '/' works too
+})
+
+// export default defineConfig(({ mode }) => ({
 //   plugins: [
 //     tailwindcss(),
 //     react()],
-//     // base: '/',
-//     // base: '/Pawthway/',
-//     base: mode === 'production' ? '/Pawthway/' : '/', // Use /Pawthway/ for deploy, / for local dev
-// })
-
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    tailwindcss(),
-    react()],
-  base: mode === 'production' ? '/Pawthway/' : '/',
-}))
+//   base: mode === 'production' ? '/Pawthway/' : '/',
+// }))
